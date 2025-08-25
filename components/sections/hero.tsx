a99456e26b47f491f44260576ef5bd6d49f89ef2
Krutagna Patel <krutagnapatel31@gmail.com>
2025-08-25 17:36:31 +0530

    add content and buid hero

---
+++ components/sections/hero.tsx
@@ -0,0 +1,40 @@
+import { SectionContainer, ViewContainer } from "@/components/layouts";
+import { Button } from "@/components/ui";
+
+function Hero() {
+  return (
+    <SectionContainer>
+      <ViewContainer className="space-y-6">
+        <div>
+          <picture>
+            <source
+              media="(min-width:768px)"
+              srcSet="/images/image-web-3-desktop.jpg"
+            />
+            <img
+              src="/images/image-web-3-mobile.jpg"
+              alt="a picture showing few shapes"
+            />
+          </picture>
+        </div>
+        <div className="grid md:grid-cols-2">
+          <h1 className="text-4xl md:text-5xl font-extrabold">
+            The Bright Future of Web 3.0?
+          </h1>
+          <div className="space-y-2">
+            <p className="text-muted-foreground">
+              We dive into the next evolution of the web that claims to put the
+              power of the platforms back into the hands of the people. But is
+              it really fulfilling its promise?
+            </p>
+            <Button className="" size="lg">
+              READ MORE
+            </Button>
+          </div>
+        </div>
+      </ViewContainer>
+    </SectionContainer>
+  );
+}
+
+export { Hero };
